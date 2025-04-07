6f892a21832fd258a20e62f102a6d01fc43d1598
fuad <fuadshorlarxwix@gmail.com>
2025-04-08 00:20:00 +0100

    nakestudio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-white bg-gradient-to-r from-purple-500 to-pink-500 p-4 rounded-lg shadow-lg">
+      Hello, Tailwind!
+    </div>
+  );
+};
+
+export default App;
