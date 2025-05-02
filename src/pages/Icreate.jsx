f6e756bf83decd667e21e7b683efc8955d01b521
fuad <fuadshorlarxwix@gmail.com>
2025-05-02 12:12:26 +0100

    more pics added to the icre8 pg

--- src/pages/Icreate.jsx
+++ src/pages/Icreate.jsx
@@ -9,19 +9,45 @@ const imgList = [
   "3copy.jpg",
   "3ocopy.jpg",
   "4copy.jpg",
+  "a.png",
+  "b.png",
+  "c.png",
+  "d.png",
+  "e.png",
+  "f.png",
+  "g.png",
+  "h.png",
+  "i.png",
+  "j.png",
+  "k.png",
+  "l.png",
+  "m.png",
+  "n.png",
+  "o.png",
+  "p.png",
+  "q.png",
+  "r.png",
+  "s.png",
+  "t.png",
+  "u.png",
+  "v.png",
 ];
 const Icreate = () => {
   return (
     <div className="p-4 md:p-8">
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 ">
         {imgList.map((img, index) => (
-          <img
+          <div
             key={index}
-            src={`/images/${img}`}
-            alt={`Image ${index}`}
-            loading="lazy"
-            className="w-full h-auto rounded "
-          />
+            className="w-full aspect-[4/3] overflow-hidden rounded"
+          >
+            <img
+              src={`/images/${img}`}
+              alt={`Image ${index}`}
+              loading="lazy"
+              className="w-full h-full object-cover "
+            />
+          </div>
         ))}
       </div>
     </div>
