5f3ca1777aed42956c1e6f52f0dde36a3873982b
fuad <fuadshorlarxwix@gmail.com>
2025-05-03 21:10:50 +0100

    grid to 1

--- src/pages/Icre8.jsx
+++ src/pages/Icre8.jsx
@@ -45,7 +45,7 @@ const Icre8 = () => {
   }));
 
   return (
-    <div className="p-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+    <div className="p-6 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
       {slides.map((slide, i) => (
         <img
           key={i}
